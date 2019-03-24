ea006e6f0475ad6e0a26531dd26f15248c1e8334
DeteMin <43286838+DeteMin@users.noreply.github.com>
2019-03-24 14:16:34 +0800

    SpringBoot整合Mybatis

---
+++ mabatis/src/main/java/com/superj/mybatis/controller/UserRestController.java
@@ -0,0 +1,26 @@
+package com.superj.mybatis.controller;
+
+import com.superj.mybatis.pojo.User;
+import com.superj.mybatis.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: SuperJ
+ * @Date: 2019/3/24 13:46
+ * @Description:
+ */
+@RestController
+public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/api/findUserById",method = RequestMethod.GET)
+    public User findUserById(@RequestParam(value = "id",required = true) Integer id){
+        return userService.findById(id);
+    }
+}
